b05494ccbf1aa4212d21ed9f715a7f16c09faf1b
Kennyegun24 <kennyegun240@gmail.com>
2025-02-08 21:40:01 +0100

    Mod

--- utils/checkRoles.js
+++ utils/checkRoles.js
@@ -1,8 +1,10 @@
+import connectMongoDb from "@/lib/mongodb";
 import Role from "@/models/Roles";
 import Teams from "@/models/Teams";
 
 export const userRolesAre = async (user_id, routePermission) => {
   try {
+    await connectMongoDb();
     const user = await Teams.findById(user_id).select("roles");
     if (!user || !user.roles.length) return false;
     const roles = await Role.find({ name: { $in: user.roles } }).select(
